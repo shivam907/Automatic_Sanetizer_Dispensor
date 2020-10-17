1e6bbd221a63c3d4c7fdf146bbd41eeb76fbb506
Shivam Kaushal <shivamkaushal907@gmail.com>
2020-10-17 10:01:14 +0530

    Update smartdustbin.ino

--- smartdustbin.ino
+++ smartdustbin.ino
@@ -1,10 +1,10 @@
 #include <Servo.h>
 int servopin = 9;
-//int ir = 12;
+int ir = 12;
 Servo servo;
 void setup()
 {
-//  pinMode(ir, INPUT);
+  pinMode(ir, INPUT);
   servo.attach(servopin);
 //  delay(100);
   servo.write(0);
@@ -16,23 +16,24 @@ void setup()
 
 void loop()
 {
-
-//if (digitalRead(ir)==0)
-//{
+Serial.print(digitalRead(ir));
+if (digitalRead(ir)==0)
+{
   servo.attach(servopin);
   delay(1000);
-  servo.write(180);
+  servo.write(90);
   delay(1000);
+//  break;
 //  servo.write(120);
-  servo.detach();
+//  servo.detach();
   
-//}
-//if (digitalRead(ir)==1)
-//{
-  servo.attach(servopin);
+}
+if (digitalRead(ir)==1)
+{
+//  servo.attach(servopin);
   delay(100);
   servo.write(0);
   delay(1000);
   servo.detach();
-//}
+}
 }
